d5946466c7d9339b87d523211e3136f037542e3f
magyarcslinux <magyarcslinux@gmail.com>
2018-03-06 20:04:14 +0100

    sss

--- x86_64/updaterepo.sh
+++ x86_64/updaterepo.sh
@@ -3,13 +3,13 @@
 # echo "Fájlok törlése"
 
 
-rm magyararcslinux_repo*
+rm magyarcs_repo*
 
 
 echo "repo-add"
-repo-add  magyararcslinux_repo.db.tar.gz *.pkg.tar.xz
+repo-add  magyarcs_repo.db.tar.gz *.pkg.tar.xz
 #sleep 8
-cp  magyararcslinux_repo.db.tar.gz magyararcslinux_repo.db
+cp  magyarcs_repo.db.tar.gz magyarcs_repo.db
 
 echo "####################################"
 echo "Repo Updated!!"
