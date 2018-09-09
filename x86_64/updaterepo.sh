13de16e0cb51a32144ac983720647587ccb96e4d
Veress Krisztián <krive001@gmail.com>
2018-09-09 09:42:40 +0200

    sasa

--- x86_64/updaterepo.sh
+++ x86_64/updaterepo.sh
@@ -7,8 +7,8 @@ rm magyarcs_repo*
 
 
 echo "repo-add"
-repo-add  magyarcs_repo.db.tar.gz *.pkg.tar.xz
-#sleep 8
+repo-add  -n   magyarcs_repo.db.tar.gz *.pkg.tar.xz
+sleep 8
 rm magyarcs_repo.db
 
 cp  -f magyarcs_repo.db.tar.gz magyarcs_repo.db
